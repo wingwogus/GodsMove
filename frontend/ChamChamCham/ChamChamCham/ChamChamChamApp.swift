eb5c5c46fc654ed7afe48c75ddfaa3c9f39ca2ad
iyungui <rsdbddml@gmail.com>
2026-07-02 13:16:25 +0900

    chore initialize project

---
+++ frontend/ChamChamCham/ChamChamCham/ChamChamChamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChamChamChamApp.swift
+//  ChamChamCham
+//
+//  Created by iyungui on 7/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ChamChamChamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
